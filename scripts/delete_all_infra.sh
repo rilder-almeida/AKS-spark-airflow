3d0320830650e7bd9a277249fa0d3d79182016ae
rilder-almeida <rilder.almeida@gmail.com>
2021-11-28 01:49:15 -0300

    fix: delete order

--- scripts/delete_all_infra.sh
+++ scripts/delete_all_infra.sh
@@ -4,5 +4,5 @@ az group delete --resource-group $TF_VAR_cluster_rg_name --yes --no-wait
 az group delete --resource-group MC_${TF_VAR_cluster_rg_name}_${TF_VAR_cluster_name}_${TF_VAR_location} --yes --no-wait
 
 
-# az group delete --resource-group $TF_VAR_datalake_rg_name --yes --no-wait
 # az storage account delete --name $TF_VAR_sa_name 
+# az group delete --resource-group $TF_VAR_datalake_rg_name --yes --no-wait
