22199918fca6a09bd784af4e9b09354a86eedb70
rilder-almeida <rilder.almeida@gmail.com>
2021-11-27 23:42:37 -0300

    fix: stop remove no wait

--- scripts/delete_all_infra.sh
+++ scripts/delete_all_infra.sh
@@ -1,4 +1,4 @@
-az aks stop --name $TF_VAR_cluster_name --resource-group $TF_VAR_rg_name --no-wait
+az aks stop --name $TF_VAR_cluster_name --resource-group $TF_VAR_rg_name
 az aks delete --name $TF_VAR_cluster_name --resource-group $TF_VAR_rg_name --yes --no-wait
 az group delete --resource-group $TF_VAR_rg_name --yes --no-wait
 az group delete --resource-group MC_${TF_VAR_rg_name}_${TF_VAR_cluster_name}_${TF_VAR_location} --yes --no-wait
