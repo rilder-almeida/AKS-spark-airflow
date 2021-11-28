767e445b59e96f8c8e8430a10d8385a7a9c0d756
rilder-almeida <rilder.almeida@gmail.com>
2021-11-27 23:42:06 -0300

    fix: stop command

--- scripts/delete_all_infra.sh
+++ scripts/delete_all_infra.sh
@@ -1,7 +1,6 @@
-az aks stop --name $TF_VAR_cluster_name --resource-group $TF_VAR_rg_name --yes --no-wait
+az aks stop --name $TF_VAR_cluster_name --resource-group $TF_VAR_rg_name --no-wait
 az aks delete --name $TF_VAR_cluster_name --resource-group $TF_VAR_rg_name --yes --no-wait
 az group delete --resource-group $TF_VAR_rg_name --yes --no-wait
-az group delete --resource-group MC_${TF_VAR_rg_name}_${TF_VAR_cluster_name} --yes --no-wait
-az group delete --resource-group NetworkWatcherRG --yes --no-wait
+az group delete --resource-group MC_${TF_VAR_rg_name}_${TF_VAR_cluster_name}_${TF_VAR_location} --yes --no-wait
 
 # az storage account delete --name $TF_VAR_datalake_name
